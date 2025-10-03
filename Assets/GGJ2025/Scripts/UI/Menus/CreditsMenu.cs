3669b64c5aa9f6892606bfc712404674c4b3fadc
FabioGiannino <fabiogiannino2@gmail.com>
2025-10-03 13:11:40 +0200

    Add new Credits scene

--- Assets/GGJ2025/Scripts/UI/Menus/CreditsMenu.cs
+++ Assets/GGJ2025/Scripts/UI/Menus/CreditsMenu.cs
@@ -1,9 +1,47 @@
+using UnityEngine;
+
 public class CreditsMenu : BaseUI {
+    [SerializeField]
+    private GameObject oldCredits;
+    [SerializeField]
+    private GameObject newCredits;
+    [SerializeField]
+    private GameObject oldCreditsButton;
+    [SerializeField]
+    private GameObject newCreditsButton;
+
+
+    private bool isOldCredits;
+
     private void OnEnable() {
         //AudioManager.PlayOneShotSound("MenuOpen");
+        oldCredits.SetActive(false);
+        oldCreditsButton.SetActive(true);
+        newCredits.SetActive(true);
+        newCreditsButton.SetActive(false);
+        isOldCredits = false;
     }
 
     private void OnDisable() {
         //AudioManager.PlayOneShotSound("MenuClose");
+    }
+
+    public void ChangeCredits()
+    {
+        if(isOldCredits)
+        {
+            oldCredits.SetActive(false);
+            oldCreditsButton.SetActive(true);
+            newCredits.SetActive(true);
+            newCreditsButton.SetActive(false);
+        }
+        else
+        {
+            oldCredits.SetActive(true);
+            oldCreditsButton.SetActive(false);
+            newCredits.SetActive(false);
+            newCreditsButton.SetActive(true);
+        }
+        isOldCredits = !isOldCredits;
     }
 }
